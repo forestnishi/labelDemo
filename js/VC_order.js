1062f11bd1ff4e0e96aa803c12f8fa81aceb4dfb
onisan <katayamaakihiko@m2.gyao.ne.jp>
2021-04-11 21:20:04 +0900

    オーダーの消し方を調整

--- js/VC_order.js
+++ js/VC_order.js
@@ -1,8 +1,6 @@
 $(function(){
 
     $(document).ready(function(){
-
-
         
     });
 
@@ -23,13 +21,43 @@ $(function(){
             total_current_order++;
             return true;
         }
+
 	});
 
     //orderを消し込む
 	$(document).on("click", ".newOrder", function(){
-        let targetid = $(this).attr('id');
-        $('#' + targetid).remove();
-        total_current_order--;
+
+        let bkcolor = $(this).css("background-color");
+
+        if( $(this).css("background-color") == "rgb(0, 255, 255)" ) {
+
+                let thisSelector = this;
+                // 対象のオーダーの背景色を変えておきもう一度トリガーをかける
+                $(this).css( {'background-color': 'orange' } );
+
+                // 色の変更が反映されるよう非同期な処理を使う
+                setTimeout(function(){
+
+                    // 対象オーダーの背景色がorangeならオーダーを消す
+                    let result = window.confirm('このオーダーを消しますか！');
+                
+                    if( result ) {
+                        $(thisSelector).animate({
+                            opacity: 0,    // 透明度0へ　徐々に消していく
+                        }, 200, function() {
+
+                            // アニメーション完了後　対象の要素を消す
+                            $(thisSelector).remove();
+                        });
+                        total_current_order--;
+                    }
+                    else {
+                        $(this).css( {'background-color': 'aqua' } );
+                    }
+
+                }, 0);
+        }
+
 	});
 
 });
